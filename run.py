c36cb2ef8f1a7db2e3a6a3eb02e73ff7fcb41f9f
Ivan <podlipnyak@econ.vsu.ru>
2020-06-02 14:36:23 +0300

    unittests

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 import numpy as np
+import unittest
+
+class TestSubnetSearch(unittest.TestCase):
+    def test_first_sub_is_common(self):
+        self.assertEqual(searchSubnet(['192.167.1.12','192.168.2.34']), '192')
+        
+    def test_second_sub_is_common(self):
+        self.assertEqual(searchSubnet(['192.168.1.12','192.168.2.34']), '192.168')
+        
+    def test_third_sub_is_common(self):
+        self.assertEqual(searchSubnet(['192.168.1.12','192.168.1.34']), '192.168.1')
+        
+    def test_no_sub_is_common(self):
+        self.assertEqual(searchSubnet(['193.168.1.12','192.168.1.34']), '')
 
 '''
 build subs matrix from ip list then check wich subs from first ip are common amongst others
@@ -7,11 +21,14 @@ def searchSubnet(ip_list):
     ip_matrix = np.array([[int(sub) for sub in ip.split('.')] for ip in ip_list])
     
     count_subs = [ip_matrix[:, index].tolist().count(sub) for index, sub in enumerate(ip_matrix[0])]
-    common_subs = [str(ip_matrix[0][index]) for index, count in enumerate(count_subs) if count == len(ip_matrix)]
+    common_subs = list(genCommonSubs(ip_matrix, count_subs, ip_matrix[0]))
     
-    return '.'.join(common_subs)
+    return '.'.join(common_subs)    
+
+def genCommonSubs(ip_matrix, count_subs, ip_sample):
+    for index, count in enumerate(count_subs):
+        if len(ip_matrix) != count: return
+        yield str(ip_sample[index])
 
 if __name__ == "__main__" :
-    ip_list = ['192.168.1.12','192.168.2.34']
-    result = searchSubnet(ip_list)
-    print(result)
+    unittest.main()
